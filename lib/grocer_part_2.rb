f56202bd209d7c29844c943a58232aee7e99631d
Evan <evanfujita@gmail.com>
2020-12-18 09:57:13 -0600

    commit tree c1d1eaa41bea3d157b9973de9d1941643a1bb1c0

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,25 +1,35 @@
 require_relative './part_1_solution.rb'
+require 'pry'
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  cart.each do |item|
+    coupons.each do |coupon|
+      if coupon[:item] == item[:item]
+        hash = {item: "#{item[:item]} W/COUPON", price: coupon[:cost]/coupon[:num], count: coupon[:num], clearance: item[:clearance] }
+        item[:count] -= coupon[:num]
+        cart << hash
+      end
+    end
+  end
 end
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  new_cart = []
+  cart.map do |item|
+    if item[:clearance]
+      item[:price] *= 0.8
+    end
+    new_cart << item
+  end
+  new_cart
 end
 
 def checkout(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # This method should call
-  # * consolidate_cart
-  # * apply_coupons
-  # * apply_clearance
-  #
-  # BEFORE it begins the work of calculating the total (or else you might have
-  # some irritated customers
+  subtotal = consolidate_cart(cart).map {|cart| cart[:price]}.sum
+  coupon_cart = apply_coupons(cart, coupons).map {|cart| cart[:price]}.sum
+  clearance_cart = apply_clearance(cart).map {|cart| cart[:price]}.sum
+  
+  
+  
 end
+
