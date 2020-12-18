c8bad112acfb1b45a6a22ea47efede76c52103f7
Evan Fujita <evanfujita@gmail.com>
2020-12-18 14:41:49 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,18 +1,67 @@
 require_relative './part_1_solution.rb'
+require 'pry'
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  hash = {}
+  coupon_cart = []
+  if coupons 
+    coupons.each do |coupon|
+      cart.collect do |item|
+        coupon[:item] == item[:item]
+      end
+      
+      coupon_applier(cart, coupons)
+          hash[:item] = "#{coupon[:item]} W/COUPON"
+          hash[:count] = coupon[:num]
+          hash[:clearance] = item[:clearance]
+          hash[:price] = coupon[:cost] / coupon[:num]
+          item[:count] -= coupon[:num]
+          
+          end
+      end    
+      end
+  end
+  coupon_cart
 end
 
+
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  clearance_cart = []  
+  new_cart = consolidate_cart(cart).each do |item|
+    if item[:clearance]
+      item[:price] -= item[:price] * 0.2
+    end
+      clearance_cart << item
+    end
+  clearance_cart
 end
 
 def checkout(cart, coupons)
+ sub_cart = []
+ new_cart = []
+ consolidated_cart = consolidate_cart(cart)
+ consolidated_cart.map do |item|
+    if item[:clearance] == "false"
+    new_cart << item[:price]
+    
+ end
+
+ #new_cart << apply_coupons(consolidated_cart, coupons)
+ #binding.pry
+ apply_clearance(cart).each do |item|
+    
+  # new_cart << item[:price]
+   #binding.pry
+  end 
+ 
+   total = new_cart.sum
+    if total > 100
+      total -= total * 0.1 
+  end
+  total
+ end
+   
+   
   # Consult README for inputs and outputs
   #
   # This method should call
@@ -22,4 +71,3 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
-end
